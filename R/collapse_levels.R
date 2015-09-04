91c1a74d3bbb4da4d080b16f933c7ba6db0447ac
evanwang1990 <evanwang1990@gmail.com>
2015-09-04 13:19:59 +0800

    buiding collapseLevel

--- R/collapse_levels.R
+++ R/collapse_levels.R
@@ -1,8 +1,4 @@
-collapseNames <- function(names, i, j)
-{
-  names[i] <- paste0(names[c(i, j)], collapse = '+')
-  names[-j]
-}
+
 
 collapseLevel <- function(x,                                # independent variable
                           y,                                # target
@@ -32,7 +28,7 @@ collapseLevel <- function(x,                                # independent variab
     mode <- 'A'
   }
 
-  if(is.numeric(x)) x <- cut(rank(x, ties.method = 'min'), breaks = levels, labels = F)
+  if(is.numeric(x)) x <- as.character(cut(rank(x, ties.method = 'min'), breaks = levels, labels = F))
 
   freqMatrix <- as.matrix(table(x, y))
   # check if there are zeros in cells
@@ -40,9 +36,20 @@ collapseLevel <- function(x,                                # independent variab
 
 
   #check nrow
+  nr <- nrow(freqMatrix)
   if(nrow(freqMatrix) > 2)
   {
-    res <- collapse(freqMatrix, method, mode)
+    trace <- matrix(nrow = nr - 1, ncol = 12)
+    trace <- Collapse(freqMatrix, trace, 1, method, mode)
+
+    #choose the best collapse
+    trace[-1, 9] <- 1 - pchisq(-2 * (trace[-1, 8] - trace[1, 9]), (nr - 2) : 1)
+    trace[-1, 11] <- 1 - 2 * abs(pnorm(trace[-1, 10]) - 0.5)
+    best_indx <- sum((trace[-1, 9] < 0.1) + (trace[-1, 11] < 0.1)  == 0) + 1
+
+    #
+    labels <- rownames(freqMatrix)
+    groups <- GetGroups(labels, trace[2:best_indx, 1], trace[2:best_indx, 2])
   }
 
 

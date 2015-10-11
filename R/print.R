7203448e60ab17ba63023316b515af6777f63ead
evanwang1990 <evanwang1990@gmail.com>
2015-10-11 23:50:23 +0800

    rewrite print function

--- R/print.R
+++ R/print.R
@@ -1,51 +1,43 @@
-
-#print_trace(deparse(substitute(x)), trace, collapsed_result, method, mode, best_indx, nr, bin_iv)
-#自变量名，trace矩阵，分层后的结果，方法，最好的分组，原始的层数,binary_split_iv
-print_trace <- function(x_name, trace, collapsed_result, method, mode, best_indx, nr, bin_iv)
+print.woe <- function(trace, best_index, binary_IV, WoE_result)
 {
-  if(!is.na(bin_iv)) bin_iv <- round((bin_iv - trace[nr - 1,4]) / bin_iv * 100, 2)
-  rownames(trace)[best_indx] <- paste0(rownames(trace)[best_indx],'*')
-
-  trace <- data.frame(trace, row.names = rownames(trace))
-  setDT(trace, keep.rownames = T)
-  setnames(trace, 'rn', 'Step  ')
-  trace[, `:=`(IV             = round(IV, 4),
-               IV_decrease     = paste0(round(IV_decrease, 2), '%'),
-               X_stat          = round(X_stat, 4),
-               C_stat          = round(C_stat, 4),
-               Adjust_lift     = round(Adjust_lift, 6),
-               Prob.LR_Chi_Sq. = round(Prob.LR_Chi_Sq., 3),
-               Prob.z_score.   = round(Prob.z_score., 3),
-               Method          = ifelse(Method == 1, 'iv',
-                                        ifelse(Method == 2, 'll', 'mo')))]
-
-  cat('Predictor = ', x_name, ', Method = ', method, ', Mode = ', mode, '\n\n', sep = '')
+  cat('Predictor = ', WoE_result$summary$var, ', Method = ', WoE_result$summary$method, ', Mode = ', WoE_result$summary$mode, '\n\n', sep = '')
   cat(paste0(rep('-', 100), collapse = ''), '\n')
-  print(trace[, c(1, 4, 5, 6, 7, 8, 9, 11, 13, 14), with = F], right = T, row.names = F)
-  if(nr > 1 && mode == 'J')
+
+  # print trace
+  if(WoE_result$summary$method != 'ctree')
   {
-    cat('\nAt last step the iv decrease ', bin_iv, '% from the maximum binary-split-iv.', sep = '')
-    if(bin_iv > 5) cat(" The collapse process seems become suboptional.\n") else cat('\n')
+    row_names <- rownames(trace)
+    row_names[best_index] <- paste0(row_names[best_index], '*')
+    width <- max(nchar(row_names))
+    row_names <- sapply(row_names, function(string) paste0(c(string, rep(' ', width - nchar(string))), collapse = ''))
+    trace <- data.frame(trace, row.names = row_names, stringsAsFactors = F)
+    setDT(trace, keep.rownames = T)
+    setnames(trace, 'rn', paste0(c('Step', rep(' ', width - 4)), collapse = ''))
+    trace[, `:=`(IV             = round(IV, 4),
+                 IV_decrease     = paste0(round(IV_decrease, 2), '%'),
+                 X_stat          = round(X_stat, 4),
+                 C_stat          = round(C_stat, 4),
+                 Adjust_lift     = round(Adjust_lift, 6),
+                 Prob.LR_Chi_Sq. = round(Prob.LR_Chi_Sq., 3),
+                 Prob.z_score.   = round(Prob.z_score., 3),
+                 Method          = ifelse(Method == 1, 'max_iv',
+                                          ifelse(Method == 2, 'max_likehood', 'linear')))]
+    cat('Trace of collapsing levels:\n\n')
+    print(trace[, c(1, 4, 5, 6, 7, 8, 9, 11, 13, 14), with = F], right = T, row.names = F)
+    if (WoE_result$summary$levels > 1 && WoE_result$summary$mode == "J")
+    {
+      cat('\nAt last step the iv decrease ', binary_IV, '% from the maximum binary-split-iv.', sep = '')
+      if (binary_IV > 5) cat(" The collapse process seems become suboptional.\n") else cat('\n')
+    }
+    cat('The best collapse is at', trace[[1]][best_index])
+    if(!WoE_result$summary$is.linear) cat(', but it seems not be linear.\n') else cat('.\n\n')
+  }else{
+    if(!WoE_result$summary$is.linear) cat('It seems not be linear using ctree split method.\n\n')
   }
-  cat('The best collapse is at', trace[[1]][best_indx])
-  if(mode == 'J' && trace[best_indx, 9, with = F] > 1e-6) cat(', but it seems not be linear.\n') else cat('.\n')
 
-  if(mode == 'A') setorder(collapsed_result, WoE)
-  collapsed_result <- rbind(collapsed_result,
-                           data.frame(band       = 'Total',
-                                      CntGood    = sum(collapsed_result$CntGood, na.rm = T),
-                                      CntBad     = sum(collapsed_result$CntBad, na.rm = T),
-                                      CntRec     = sum(collapsed_result$CntRec, na.rm = T),
-                                      PctRec     = '100%',
-                                      GoodRate   = paste0(round(sum(collapsed_result$CntGood, na.rm = T) / sum(collapsed_result$CntRec, na.rm = T) * 100, 2), '%'),
-                                      BadRate    = paste0(round(sum(collapsed_result$CntBad, na.rm = T) / sum(collapsed_result$CntRec, na.rm = T) * 100, 2), '%'),
-                                      WoE        = NA,
-                                      IV         = sum(collapsed_result$IV),
-                                      WoE_barplot= ''
-                           ))
-
-  cat("\n\nBest Collapsing Result\n")
-  print(collapsed_result)
+  #print WoE_result
+  cat('The result of WoE:\n')
+  print(WoE_result$detail, right = T, row.names = F)
   cat('\n')
   cat(paste0(rep('=', 100), collapse = ''), '\n\n')
 }

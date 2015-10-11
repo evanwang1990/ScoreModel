d1b532f219869202714cbf7ea47ab600ff5edc41
evanwang1990 <evanwang1990@gmail.com>
2015-10-11 22:40:08 +0800

    remove

--- R/barplot.R
+++
@@ -1,9 +0,0 @@
-barplot.woe <- function(woe)
-{
-  woe <- scale(woe, center = T, scale = F)
-  woe[is.na(woe)] <- 0
-  woe <- round(woe / max(abs(woe)) * 5)
-  res <- sapply(woe, function(x) ifelse(x < 0, paste0(c(rep(' ', 5 + x), rep('*', -x), '|', rep(' ', 5)), collapse = ''),
-                                               paste0(c(rep(' ', 5), '|', rep('*', x), rep(' ', 5 - x)), collapse = '')))
-  res
-}

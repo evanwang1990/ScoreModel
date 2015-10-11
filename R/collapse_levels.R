39304ceec1f55e08cb0263d1367562459f05f345
evanwang1990 <evanwang1990@gmail.com>
2015-10-11 23:51:27 +0800

    rewrite part of collapse_levels function

--- R/collapse_levels.R
+++ R/collapse_levels.R
@@ -3,30 +3,29 @@
 #'df <- data.frame(x1 = sample(1:1000, 1e4, replace = T),
 #'                 x2 = sample(c(letters[1:3], NA), 1e4, replace = T),
 #'                 y  = c(sample(c(1, 1, 0), 5e3, replace = T), sample(c(1, 0, 0, 0), 5e3, replace = T)))
-#'res <- bestCollapse(c('x1', 'x2'), y, df, 20, method = 'iv', mode = 'J', tracefile = 'trace.Rout', sqlfile = 'sql_code.sql')
-#'collapseLevel(x = df$x2, y = df$y, levels = 20, method = 'iv', mode = 'A', minp = 0.05, sourcefile = 'test.R', sqlfile = 'test.sql')
+#'res <- bestCollapse(c('x1', 'x2'), y, df, 20, method = 'max_iv', mode = 'J', tracefile = 'trace.Rout', sqlfile = 'sql_code.sql')
+#'collapseLevel(x = df$x2, y = df$y, levels = 20, method = 'max_iv', mode = 'A', minp = 0.05, sourcefile = 'test.R', sqlfile = 'test.sql')
 
 #setcolorder
 #对于其中的缺失值,求WOE
 #WOE计算反了
 
+band.collapse <- function(x, x_, band_)
+{
+  if(!is.numeric(x)) return(band_)
+  range_ <- sapply(band_, function(ranges) round(max(x[x_ %in% eval(parse(text = paste0(c('c(', ranges, ')'), collapse = '')))]), 2))
+  res <- paste(c(' ', range_[-length(range_)]), c(range_[-length(range_)], ' '), sep = ' ~ ')
+  res
+}
 
-
-
-collapseLevel <- function(x,                                # independent variable
-                          y,                                # target
-                          levels,                           # initial levels
-                          method,                           # collapse methods
-                          mode,                             # collapse mode
-                          minp = 0.05                       # min percentage of the sample numbers which one level contains(include missing values)
-                          )
+collapseLevel <- function(x, y, org.levels, method, mode, minp = 0.05, ...)
 {
   if(is.character(x) || (is.factor(x) && !is.ordered(x)))
   {
-    if(method == 'mo')
+    if(method == 'linear')
     {
-      warning("Independent variable is character vector or unordered vector, 'mo' method is not appreciate\n The method is modified into 'iv' automatically.")
-      method <- 'iv'
+      warning("Independent variable is character vector or unordered vector, 'linear' method is not appreciate\n The method is modified into 'max_iv' automatically.")
+      method <- 'max_iv'
     }
     mode <- 'A'
   }else{
@@ -52,7 +51,6 @@ collapseLevel <- function(x,                                # independent variab
   # deal with the zero cells
   if(any(freqMatrix == 0))
   {
-    #collapse levels
     freqMatrix_nonzero <- CollapseZeroCells(freqMatrix, matrix(NA, ncol = 2, nrow = nrow(freqMatrix)), mode = mode)
     # after processing, check if there is still zero cells
     if(any(freqMatrix_nonzero$freqMatrix == 0))
@@ -67,7 +65,7 @@ collapseLevel <- function(x,                                # independent variab
                                          linear      = NA,
                                          suboptional = NA,
                                          method      = method,
-                                         detail      = 'zero-cell')))
+                                         detail      = 'zero cells')))
     }
 
     #update labels of freqMatrix
@@ -83,7 +81,7 @@ collapseLevel <- function(x,                                # independent variab
     trace <- matrix(nrow = nr,
                     ncol = 13,
                     dimnames = list(paste("Step", 0:(nr - 1)),
-                                    c('Left', 'Right', 'minCount', 'IV', 'IV_decrease', 'X_stat', 'C_stat', 'Adjust_lift', 'Log_likehood', 'Prob(LR_Chi_Sq)', 'Z_score_of_log_odds_ratio', 'Prob(z_score)', 'Method')))
+                                    c('Left', 'Right', 'minCount', 'max_iv', 'IV_decrease', 'X_stat', 'C_stat', 'Adjust_lift', 'Log_likehood', 'Prob(LR_Chi_Sq)', 'Z_score_of_log_odds_ratio', 'Prob(z_score)', 'Method')))
     trace <- Collapse(freqMatrix, trace, 1, method, mode)
 
     #choose the best collapse
@@ -101,56 +99,31 @@ collapseLevel <- function(x,                                # independent variab
     }
     collapsed_result <- data.table(collapsed_result, keep.rownames = T)
     setnames(collapsed_result, c('band', 'CntGood', 'CntBad'))
-    collapsed_result[, `:=`(band         = band.collapse(x, x_, band),
-                            CntRec       = CntGood + CntBad)
-                     ][, `:=`(PctRec     = paste0(round(CntRec / length(x) * 100, 2), '%'),
-                              GoodRate   = paste0(round(CntGood / CntRec * 100, 2), '%'),
-                              BadRate    = paste0(round(CntBad / CntRec * 100, 2), '%'),
-                              WoE        = round(log(CntGood / sum(y == 0)) - log(CntBad / sum(y == 1)), 4),
-                              IV         = round((CntGood / sum(y == 0) - CntBad / sum(y == 1)) * (log(CntGood / sum(y == 0)) - log(CntBad / sum(y == 1))), 4))]
-    if(any(is.na(x))) #add missing values
-    {
-      NA_good <- sum(is.na(x) & y == 0)
-      NA_bad  <- sum(is.na(x) & y == 1)
-      NA_tot <- NA_good + NA_bad
+    collapsed_result[, band := band.collapse(x, x_, band)]
+    setorder(collapsed_result, c('band', 'CntGood', 'CntBad'))
+    if (any(is.na(x)))
+        collapsed_result <- cbind(collapsed_result,
+                                  data.frame(band    = 'missing',
+                                             CntGood = sum(is.na(x) & y == 0),
+                                             CntBad  = sum(is.na(x) & y == 1)))
+    collapsed_result <- detail.woe(collapsed_result, mode)
 
-      collapsed_result <- rbind(collapsed_result,
-                               data.frame(band       = 'missing',
-                                           CntGood    = NA_good,
-                                           CntBad     = NA_bad,
-                                           CntRec     = NA_tot,
-                                           PctRec     = paste0(round(NA_tot / length(x) * 100, 2), '%'),
-                                           GoodRate   = paste0(round(NA_good / NA_tot * 100, 2), '%'),
-                                           BadRate    = paste0(round(NA_bad / NA_tot * 100, 2), '%'),
-                                           WoE        = ifelse(NA_good * NA_bad == 0, NA, round(log(NA_good / sum(y == 0)) - log(NA_bad / sum(y == 1)), 4)),
-                                           IV         = ifelse(NA_good * NA_bad == 0, NA, round((NA_good / sum(y == 0) - NA_bad / sum(y == 1)) * (log(NA_good / sum(y == 0)) - log(NA_bad / sum(y == 0))), 4))
-                                           ))
-    }
-
-    collapsed_result[, WoE_barplot := barplot.woe(WoE)]
-
+    args <- list(...)
+    IV_ctree <- args[['IV_ctree']]
+    WoE_result <- list('summary' = data.frame('var'             = deparse(substitute(x)),
+                                              'class'          = class(x),
+                                              'PctNA'          = round(sum(is.na(x)) / length(x), 3),
+                                              'levels'         = nrow(collapsed_result) - 1 - any(is.na(x)),
+                                              'IV'             = max(collapsed_result$IV),
+                                              'IV_decrease'    = ifelse(is.null(IV_ctree), NA, round((max(collapsed_result$IV) - IV_ctree) / max(collapsed_result$IV), 3)),
+                                              'is.linear'      = ifelse(mode == 'J', trace[best_indx, 8] < 1e-6, NA),
+                                              'is.suboptional' = ifelse(mode == 'J', round((binary_IV - trace[nr - 1,4]) / bin_iv * 100, 1e-3), NA),
+                                              'method'         = method,
+                                              'mode'           = mode,
+                                              'detail'         = ''),
+                       'detail'  = collapsed_result[1:(nrow(collapsed_result) - 1)])
     #print trace----
-    print_trace(deparse(substitute(x)), trace, collapsed_result, method, mode, best_indx, nr, bin_iv)
-
-
-    #stringcode <- stringCode(x, y, x_, deparse(substitute(x)), groups, !missing(sqlfile), method)
-
-    #output
-    #writeLines(stringcode[1], sourcefile, sep = ',\n')
-    #if(!missing(sqlfile)) writeLines(stringcode[2], sqlfile)
-
-
-    return(list("need_mo" = (mode == 'J' && method != 'mo' && trace[best_indx, 8] > 1e-6),
-                "main"    = data.frame(var         = deparse(substitute(x)),
-                                       class       = class(x),
-                                       NAs         = sum(is.na(x))/length(x),
-                                       iv          = round(trace[best_indx, 4], 1e-4),
-                                       levels      = nrow(collapsed_result) - any(is.na(x)),
-                                       linear      = ifelse(mode == 'J', trace[best_indx, 8] < 1e-6, NA),
-                                       suboptional = ifelse(mode == 'J', round((bin_iv - trace[nr - 1,4]) / bin_iv * 100, 1e-3), NA),
-                                       method      = method,
-                                       detail      = 'normal',
-                                       row.names   = NULL)))
+    print.woe(trace = trace, best_index = best_indx, binary_IV = binary_IV, WoE_result = WoE_result)
 
   }else if(any(is.na(x))){  #the one level variables make sense only when they have missing values(just as two levels)
     stringcode <- stringCode(x, y, x_, deparse(substitute(x)), paste0("'",rownames(freqMatrix),"'"), !missing(sqlfile), method)
@@ -167,4 +140,10 @@ collapseLevel <- function(x,                                # independent variab
                                        method      = method,
                                        detail      = 'one-level')))
   }
+}
+
+
+test <- function(...)
+{
+  list(...)
 }

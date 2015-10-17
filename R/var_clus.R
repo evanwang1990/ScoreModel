5da47e2c08225d19b6de14ce291ffd28ff66f0d2
evanwang1990 <evanwang1990@gmail.com>
2015-10-17 23:15:07 +0800

    add evaluation method

--- R/var_clus.R
+++ R/var_clus.R
@@ -13,6 +13,38 @@ twoCluster <- function(matrix, maxeigen)
   return(abs(cor_y1) > abs(cor_y2))
 }
 
+multipleR2 <- function(cluster, in_vars, all_vars, df)
+{
+  res <- matrix(NA, ncol = 4, nrow = length(in_vars), dimnames = list(in_vars, c('Cluster', 'SquaredR.own', 'SquaredR.next', '1-SquaredR Ratio')))
+  res[, 1] <- cluster
+  for (var in in_vars)
+  {
+    TSS <- sum(df[[var]]**2)
+
+    if (length(in_vars) == 1)
+      res[var, 2] <- 1
+    else{
+      formula1 <- as.formula(paste(var, '~', paste(setdiff(in_vars, var), sep = ' + ')))
+      r1 <- residuals(lm(formula1, df))
+      RSS1 <- sum(r1^2)
+      res[var, 2] <- (TSS - RSS1) / TSS
+    }
+
+    if (length(in_vars) == length(var_names))
+    {
+      res[var, 3] <- 0
+      res[var, 4] <- 0
+    }else{
+      formula2 <- as.formula(paste(var, '~', paste(setdiff(all_vars, in_vars), sep = ' + ')))
+      r2 <- residuals(lm(formula2, df))
+      RSS2 <- sum(r2^2)
+      res[var, 3] <- (TSS - RSS2) / TSS
+      res[var, 4] <- (1 - res[var, 2]) / (1 - res[var, 3])
+    }
+  }
+  res
+}
+
 varClus <- function(df, maxeigen, maxiter)
 {
   var.char <- vector("character")
@@ -42,39 +74,48 @@ varClus <- function(df, maxeigen, maxiter)
 
   if (missing(maxiter)) maxiter <- length(df) - 1
   clus <- list()
-  clus[['root']] <- list(vars = 1:length(var_names),
-                         need.deal = 1)
+  clus[['node0']] <- list(vars     = 1:length(var_names),
+                         need.deal = TRUE,
+                         is.leaf   = TRUE)
 
-  if (ncol(df) == 1) return(list(clus = clus,
+  if (ncol(df) == 1) return(list(clusinfo = c(Cluster = 1, SquaredR.own = 1, SquaredR.next = 0, `1-SquaredR Ratio` = 0),
                                  var.exclude = list(var.char     = var.char,
                                                     var.nearzero = var.nearzero)))
 
   for(i in 1:maxiter)
   {
-    deal.num = 0
-    for(j in length(clus))
+    deal.num <- 0
+    for(j in 1:length(clus))
     {
       nodes <- clus[[j]]
-      if (nodes$need.deal == 0) next
+      if (!nodes$need.deal) next
 
       twoclus <- twoCluster(df[,nodes$vars], maxeigen)
 
       if (is.null(twoclus) || all(twoclus))
       {
-        nodes$need.deal == 0
+        clus[[j]][['need.deal']] <- FALSE
         next
       }
 
       clus[[paste0('node', i, '.1')]] <- list(vars      = nodes$vars[twoclus],
-                                              need.deal = ifelse(sum(twoclus) == 1, 0, 1))
+                                              need.deal = ifelse(sum(twoclus) == 1, FALSE, TRUE),
+                                              is.leaf   = TRUE)
       clus[[paste0('node', i, '.2')]] <- list(vars      = nodes$vars[!twoclus],
-                                              need.deal = ifelse(sum(!twoclus) == 1, 0, 1))
-      clus[[j]][["need.deal"]] <- 0
+                                              need.deal = ifelse(sum(!twoclus) == 1, FALSE, TRUE),
+                                              is.leaf   = TRUE)
+      clus[[j]][["need.deal"]] <- FALSE
+      clus[[j]][['is.leaf']] <- FALSE
       deal.num <- 1
     }
     if (deal.num == 0) break
   }
-  return(list(clus = clus,
+
+  leaves <- which(unlist(lapply(clus, `[`, 3)))
+  df <- as.data.frame(df)
+  res <- lapply(1:length(leaves), function(i){node <- clus[[leaves[i]]]; multipleR2(i, var_names[node$vars], var_names, df)})
+  res <- Reduce(rbind, res)
+  return(list(clusinfo = res,
               var.exclude = list(var.char     = var.char,
                                  var.nearzero = var.nearzero)))
 }
